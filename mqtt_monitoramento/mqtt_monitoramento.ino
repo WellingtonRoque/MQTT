1a456b0d246e00d17c8ae85f393485cf3980c984
WellingtonRoque <78979041+WellingtonRoque@users.noreply.github.com>
2023-08-28 19:10:31 -0300

    Add files via upload

---
+++ mqtt_monitoramento/mqtt_monitoramento.ino
@@ -0,0 +1,80 @@
+#include <PubSubClient.h>
+#include <WiFi.h>
+#include "DHT.h"
+
+const char* ssid = "WAM 2.4";
+const char* password ="manu2020";
+const char* mqttServer = "broker.hivemq.com";
+const int mqttPort = 1883;
+const char* mqttUser = "";
+const char* mqttPassword = "";
+
+const char* temperatureTopic = "casa/sensor/temperatura";
+const char* humidityTopic = "casa/sensor/umidade";  
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+#define DHT_PIN 12 // Substitua pelo pino do sensor DHT
+#define DHT_TYPE DHT11
+
+DHT dht(DHT_PIN, DHT_TYPE);
+
+void setup() {
+    dht.begin();
+    Serial.begin(115200);
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+       Serial.print("Connecting to WiFi:");
+        Serial.println(ssid);
+    }
+
+    Serial.println("Connected to the WiFi network");
+    Serial.println("");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+    client.setServer(mqttServer, mqttPort);
+    //client.setCallback(callback);
+
+    while (!client.connected()) {
+        Serial.println("Connecting to MQTT…");
+        String clientId = "ESP32Client-";
+        clientId += String(random(0xffff), HEX);
+        if (client.connect(clientId.c_str(), mqttUser, mqttPassword )) {
+            Serial.println("connected");
+        } else {
+            Serial.print("failed with state ");
+            Serial.print(client.state());
+            delay(2000);
+        }
+    }
+
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+
+    Serial.print("Message arrived in topic: ");
+    Serial.println(topic);
+
+    Serial.print("Message:");
+
+}
+
+void loop() {
+    client.loop();
+
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
+  Serial.println(temperature);
+  Serial.println(humidity);
+
+  if (!isnan(temperature) && !isnan(humidity)) {
+    String tempPayload = String(temperature);
+    String humPayload = String(humidity);
+    client.publish(temperatureTopic, tempPayload.c_str());
+    client.publish(humidityTopic, humPayload.c_str());
+  }
+  delay(1000);
+}
